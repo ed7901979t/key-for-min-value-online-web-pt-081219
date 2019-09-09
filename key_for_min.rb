accd63d072b38bd64b23a222e970b2bfb00a0209
clean-command-1851 <ed7901979t@gmail.com>
2019-09-09 22:32:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  smallest_key = nil
+  tiny_value = nil
+  hash.each do |key, value|
+    if tiny_value == nil || value < tiny_value
+      tiny_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
 end
+
+key_for_min_value(hash)
